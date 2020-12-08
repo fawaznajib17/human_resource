9962088ed823d911a6fdf763e6993a2e8b382579
fawaznajib17 <51849670+fawaznajib17@users.noreply.github.com>
2020-12-09 00:23:47 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width='device-width', initial-scale=1.0">
+    <title>Human Resource</title>
+</head>
+
+<body>
+    <h1>selamat datang</h1>
+    <div>
+        <a href="karyawanpost.php">post karyawan</a>
+        <a href="karyawanget.php">get karyawan</a>
+    </div>
+</body>
+
+</html>
