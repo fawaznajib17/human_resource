a65d18788ae6f51b4f7e6f8fc103be4504d63b25
fawaznajib17 <51849670+fawaznajib17@users.noreply.github.com>
2020-12-09 01:09:21 +0700

    Update karyawanapost.php

--- karyawanapost.php
+++ karyawanapost.php
@@ -1,3 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width='device-width', initial-scale=1.0">
+    <title>Human Resource</title>
+</head>
+
+<body>
+    <h1>ambil dipostman</h1>
+    <div>
+    </div>
+</body>
+
+</html>
 <?php
 $arr = json_decode(file_get_contents("php://input"));
 
