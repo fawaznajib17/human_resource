95e34b766a083691662bbb8b7ebe5b75cbd959c7
fawaznajib17 <51849670+fawaznajib17@users.noreply.github.com>
2020-12-09 00:34:24 +0700

    Update koneksi.php

--- koneksi.php
+++ koneksi.php
@@ -1,8 +1,8 @@
 <?php
 
-$host = "localhost";
-$user = "root";
-$pass = "";
+$host = "https://www.db4free.net/phpMyAdmin/server_databases.php";
+$user = "fawaz";
+$pass = "fawaznajib17";
 $database = "human_resource";
 $koneksi = mysqli_connect ($host, $user, $pass, $database);
 if($koneksi){
